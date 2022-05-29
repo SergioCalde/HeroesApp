c80b938b4bf8d4cf6e24f1678f1bcb836af73c21
Sergio Calderon <sergio.calderon.dm@outlook.com>
2022-05-29 17:28:33 -0600

    Creación de CRUD del App de Héroes

---
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from "rxjs";
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html',
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `
+  ]
+})
+export class AgregarComponent implements OnInit {
+
+  action: string = 'Nuevo';
+
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ]
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  }
+
+  constructor( private heroesService: HeroesService,
+                private activatedRoute: ActivatedRoute,
+                private router: Router) { }
+
+  ngOnInit(): void {
+
+
+    if( !this.router.url.includes('editar')){
+      this.action = 'Nuevo';
+      return;
+    }
+
+    this.action = 'Editar';
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.heroesService.getHeroesPorId(id))
+    )
+      .subscribe( heroe => this.heroe = heroe )
+
+  }
+
+  guardar() {
+
+    if( this.heroe.superhero.trim().length === 0){
+      return;
+    }
+
+    if( this.heroe.id ){
+      //Actualizar
+      this.heroesService.actualizarHeroe( this.heroe )
+        .subscribe( heroe => console.log('Actualizando', heroe))
+    }else{
+      this.heroesService.agregarHeroe( this.heroe )
+        .subscribe( heroe => {
+          this.router.navigate(['/heroes/editar', heroe.id])
+        } )
+    }
+  }
+
+  borrarHeroe(){
+    
+    this.heroesService.borrarHeroe( this.heroe.id! )
+      .subscribe( resp => {
+        this.router.navigate(['/heroes']);
+      });
+
+  }
+}
